2705f72d27bd99fa5e78b5dd98c12a053e79638f
Roman "Valberton" Levchencko <romandogmc@gmail.com>
2025-03-22 17:46:04 +0300

    22.03.25

---
+++ python/hex2uint32.py
@@ -0,0 +1,16 @@
+# py3
+
+def hex2uint(hex_string):
+    return int('0x%s' % hex_string, 0) if not hex_string.startswith('0x') else int(hex_string, 0)
+
+def uint2hex(integer):
+    return hex(int(integer))
+
+while True:
+    mode = input('Режим? (H)ex/(I)nt > ')
+    inputData = input('Значение? > ')
+    if mode.lower() == 'h':
+        print('hex', hex2uint(inputData))
+    elif mode.lower() == 'i':
+        print('int', uint2hex(inputData))
+    print('------------------')
